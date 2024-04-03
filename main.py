94cdb5584903f95f4f52c8c43c49d9e07d1e7adc
deangeckt <dgeckt@gmail.com>
2024-04-03 10:53:20 +0300

    cook 19 - examploe network to compare with mfinder - remove the node 0

--- main.py
+++ main.py
@@ -133,9 +133,11 @@ if __name__ == "__main__":
     # custom_graph_search(nx.DiGraph([(1, 0), (2, 0), (1, 2)]))
 
     # motif_search("networks/toy_network.txt", "toy")
-    motif_search("networks/Uri_Alon_2002/example.txt", "paper example")
+    # motif_search("networks/Uri_Alon_2002/example.txt", "paper example")
 
     # motif_search("networks/Uri_Alon_2002/coliInterNoAutoRegVec.txt", "colinet1_noAuto")
+    motif_search("networks/Cook_2019/2020_si_2_herm_chem_synapse_adj_5.txt",
+                 '2020_si_2_herm_chem_synapse_adj_5')
 
     # motif_search("networks/Cook_2019/2020_si_2_herm_chem_synapse_adj_5.txt",
     #              "2020_si2_herm_chem_synapse",
