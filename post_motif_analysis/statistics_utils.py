4b0c3a88efa44618f92fd4e4c133623ea20688eb
deangeckt <dgeckt@gmail.com>
2024-09-01 14:08:10 +0300

    z score bootstrap: fix with while loop and stop criteira, analysis: add distribution plot

--- post_motif_analysis/statistics_utils.py
+++ post_motif_analysis/statistics_utils.py
@@ -11,11 +11,19 @@ def z_score_bootstrap(motif: Motif, iterations=10000, iteration_sample=1000) -> 
     :return: z score distribution (list of floats)
     """
     z_scores = []
-    for _ in range(iterations):
+    sum_ = 0
+    warn_amount_max = iterations / 2
+    warn_sum_ = 0
+    while sum_ < iterations:
         samples = np.random.choice(motif.random_network_samples, size=iteration_sample, replace=True)
         n_rand = np.mean(samples)
         std = np.std(samples)
         if not std:
+            warn_sum_ += 1
+            if warn_sum_ == warn_amount_max:
+                print(f'motif id: {motif.id} err: std was 0, {warn_amount_max} times! z scores len is: {len(z_scores)}')
+                return z_scores
             continue
         z_scores.append((motif.n_real - n_rand) / std)
+        sum_ += 1
     return z_scores
