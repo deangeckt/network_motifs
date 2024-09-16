7d74a01cb84de5980ad1d4fb978655cba9e2d4fd
Sagi Levy Lab <slevylab@technion.ac.il>
2024-09-16 11:49:05 +0300

    fix bat file

--- scripts/single_run.bat
+++ scripts/single_run.bat
@@ -1,7 +1,7 @@
 @echo off
 python motif_search_main.py ^
 --input_type durbin_txt ^
---input_network_file "networks/data/Durbin_1986/neurodata.txt" ^
+--input_network_file "../networks/data/Durbin_1986/neurodata.txt" ^
 --synapse_threshold 10 ^
 --filter_sex_type herm ^
 --filter_syn_type chem ^
